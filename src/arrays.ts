1b9b5b3cfea4167cb8b6971a1244e22f6ac7b904
Shaina <shainaz@udel.edu>
2025-02-22 15:08:45 -0500

    added makeMath

--- src/arrays.ts
+++ src/arrays.ts
@@ -95,7 +95,11 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    return "";
+    const sum = addends.reduce((total: number, num: number) => total + num, 0);
+    if (addends.length === 0) {
+        return "0=0";
+    }
+    return sum + "=" + addends.join("+");
 }
 
 /**
